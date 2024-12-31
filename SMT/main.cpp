59512082e24afa7e8bcf9ebbb0d004109fb1b91d
yeongjinjeong <agw1247@gmail.com>
2024-12-31 06:55:03 +0000

    Update main.cpp

--- SMT/main.cpp
+++ SMT/main.cpp
@@ -1,22 +1,43 @@
+#include "Process/include/ProcessManager.h"
 #include <iostream>
-#include "function1.h"
-#include "function2.h"
+#include <memory>
 
-int main(){
-    Function1 func1;
-    Function2 func2;
+void showMainMenu() {
+    std::cout << "\n=== System Management Tool ===\n"
+              << "1. Process Management\n"
+              << "2. Network Management\n"
+              << "3. Storage Management\n"
+              << "0. Exit\n"
+              << "Choose an option: ";
+}
 
-    int num;
-    //추후에 번호 말고 명령어 형식으로 바꿔야함.
-    //그리고 나중에는 makefile 만들어서 컴파일.
-    while(true){
+int main() {
+    int choice;
+    std::unique_ptr<ProcessManager> processManager;
 
-        std::cin >> num;
+    while (true) {
+        showMainMenu();
+        std::cin >> choice;
 
-        if(num==0)break;
-        else if(num==1)func1.doSomething();
-        else if(num==2)func2.doSomething2();
-        else std::cout << "Invalid Input" << std::endl;
+        switch (choice) {
+            case 1:
+                if (!processManager) {
+                    processManager = std::make_unique<ProcessManager>();
+                }
+                processManager->run();
+                break;
+            case 2:
+                std::cout << "Network Management - Not implemented yet\n";
+                break;
+            case 3:
+                std::cout << "Storage Management - Not implemented yet\n";
+                break;
+            case 0:
+                std::cout << "Exiting...\n";
+                return 0;
+            default:
+                std::cout << "Invalid option!\n";
+        }
     }
 
     return 0;
